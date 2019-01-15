8d1968e3698b1b59b8aed2b39c7d2b0077101f6a
makhnoart <makhnoarthur@gmail.com>
2019-01-15 19:29:59 +0200

    setting mysite way css

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^library/', include('library.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^', include('library.urls')),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
+
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
-
-from django.template import RequestContext, loader
+from django.template import loader
 
 from .models import Question
 
@@ -8,10 +7,8 @@ from .models import Question
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_question_list': latest_question_list,
-    })
-    return HttpResponse(template.render(context))
+    context = {'latest_question_list': latest_question_list,}
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
