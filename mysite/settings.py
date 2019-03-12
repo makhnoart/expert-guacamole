77f556d9c13f61bb0de4e302a2cd83ee2e66bd7e
makhnoart <makhnoarthur@gmail.com>
2019-03-12 19:34:41 +0200

    delete celery _beat and _results in INSTALLED_APPS

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_celery_beat',
-    'django_celery_results',
 
     'library',
     'polls',
