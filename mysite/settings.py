d8466eaeae7722249388f9f1bcfbc6fd8f7f7b7c
makhnoart <makhnoarthur@gmail.com>
2019-03-12 19:14:58 +0200

    add celery to project

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_celery_beat',
+    'django_celery_results',
 
     'library',
     'polls',
@@ -141,6 +143,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# REDIS related settings
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = TIME_ZONE
+
 
 # Email settings changes
 EMAIL_USE_TLS = True
